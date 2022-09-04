22ecb4616e726feac4ed0a3426f20663f88ffce4
kickscar <kickscar@gmail.com>
2022-09-04 13:19:15 +0900

    analytic #5

--- tools/make.exercise.dict.py
+++ tools/make.exercise.dict.py
@@ -11,6 +11,8 @@ def main():
     with open(dictfile, 'wb') as fdict:
         pickle.dump(exercisedict, fdict, -1)
 
+    print(f'Pickling the Exercise Dictionary Completed...[{dictfile}]')
+
 
 exercisedict = {
     "equipment-dialects": {
@@ -23,43 +25,43 @@ exercisedict = {
     "exercises": {
         "Arnold Press": {
             "dialects": [],
-            "body-groups": ("Upper Body", "Shoulder"),
+            "body-group": "Shoulder",
             "muscle-groups": ("Chest",),
             "record-type": ("Weight", "Repetition", "Set")
         },
         "Assisted Dip": {
             "dialects": ["Assisted Dips"],
-            "body-groups": ("Upper Body", "Chest"),
+            "body-group": "Chest",
             "muscle-groups": ("Chest",),
             "recrod-type": ("Weight", "Repetition", "Set")
         },
         "Assisted Pull-Up": {
             "dialects": ["Assisted Pullup"],
-            "body-groups": ("Upper Body", "Back"),
+            "body-group": "Back",
             "muscle-groups": ("Lats",),
-            "recrod-type": ["Weight", "Repetition", "Set"]
+            "recrod-type": ("Weight", "Repetition", "Set")
         },
         "Back Extension": {
             "dialects": ["Back Extensions"],
-            "body-groups": ("Upper Body", "Back"),
+            "body-group": "Back",
             "muscle-groups": ("Back",),
             "recrod-type": ("Repetition", "Set")
         },
         "Body-Weight Squat": {
             "dialects": ["Basic Squat", "Basic Squats", "Air Squat"],
-            "body-groups": ("Lower Body", ),
+            "body-group": "Leg",
             "muscle-groups": ("Quadriceps",),
             "recrod-type": ("Repetition", "Set")
         },
         "Power Leg Press": {
             "dialects": ["Power Leg Pres"],
-            "body-groups": ("Lower Body", "Leg"),
+            "body-groups": "Leg",
             "muscle-groups": ("Quadriceps",),
             "recrod-type": ("Weight", "Repetition", "Set")
         },
         "Pull-Up": {
             "dialects": ["Pull Up", "Pullup"],
-            "body-groups": ("Upper Body", "Chest"),
+            "body-groups": "Back",
             "muscle-groups": ("Quadriceps",),
             "recrod-type": ("Weight", "Repetition", "Set")
         }
