fe3636f1ae2f740cf5ca80789e1b4db3bcd2fa34
kickscar <kickscar@gmail.com>
2022-10-15 10:44:21 +0900

    .

--- tools/make.exercise.dict.py
+++ tools/make.exercise.dict.py
@@ -16,7 +16,7 @@ def main():
 
 exercisedict = {
     "equipment-dialects": {
-        "Dumbbell": [],
+        "Dumbbell": ["Dumbbel"],
         "Machine": ["Machin"],
         "Smith Machine": ["Machine, Smith"],
         "EZ Curl Bar": ["Ez Bar", "Ezbar", "Ez Curl Bar"],
